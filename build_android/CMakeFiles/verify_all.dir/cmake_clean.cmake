d59be9ae686eb19fed3ea82564b4f01b719e55ce
Petalrain224 <3763007762@qq.com>
2026-01-02 18:02:26 +0800

    Delete build_android directory

--- build_android/CMakeFiles/verify_all.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/verify_all"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/verify_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
